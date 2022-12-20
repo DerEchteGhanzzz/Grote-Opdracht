fa08affd977981b6ca4797eb22cb3de775c25173
DerEchteGhanzzz <79211646+DerEchteGhanzzz@users.noreply.github.com>
2022-12-20 12:44:06 +0100

    Node update zodat zijn order hem toevoegt

--- GroteOpdracht/Node.cs
+++ GroteOpdracht/Node.cs
@@ -1,24 +1,34 @@
-﻿namespace GroteOpdracht;
+namespace GroteOpdracht;
 
 public class Node
 {
-    public Node? Prev;
-    public Node? Next;
+    public Node Prev;
+    public Node Next;
     public Order Order;
-    
+
+    public int DayIndex;
+    public int Truck;
+    public int TripIndex;
     public int NodeIndex;
     public Node(Order order, int dayIndex, int truck, int tripIndex, int nodeIndex)
     {
         this.Order = order;
-        if (nodeIndex != -1)
-            order.AssignNode(dayIndex, truck, tripIndex, nodeIndex);
+        NodeIndex = nodeIndex;
+        DayIndex = dayIndex;
+        Truck = truck;
+        TripIndex = tripIndex;
+        if (order.OrderID != 0)
+        {
+            order.AssignNode(this);
+        }
     }
     public void Delete()
     {
+        
         Prev.Next = Next;
         if (Next is not null) Next.Prev = Prev; // check if the next or next node even exists
         
-        Order.ResetAssignedNodes(); // order only lowers the nodeCount because all nodes will get removed
+        Order.ClearNodes(); // order resets the nodeCount because all nodes will get removed
     }
 
     public void InsertNew(Order newOrder, int dayIndex, int truck, int tripIndex, int nodeIndex)
